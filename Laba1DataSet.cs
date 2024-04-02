83f1d4554e89212fcc2ebe6e40cfd6187a2853dd
ZarikZa <schemetov.ya@yandex.ru>
2024-04-02 07:17:19 +0300

    postavte 5

---
+++ Laba1DataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace Laba3DataSet
+{
+
+
+    partial class Laba1DataSet
+    {
+    }
+}
+
+namespace Laba3DataSet.Laba1DataSetTableAdapters {
+    
+    
+    public partial class OrdersTableAdapter {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Laba3DataSet.Laba1DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba3DataSet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OrdersTableAdapter OrdersTableAdapter = new OrdersTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            datygridy.ItemsSource = OrdersTableAdapter.GetDataBy();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            datygridy.Columns[0].Visibility = Visibility.Collapsed;
+            datygridy.Columns[1].Visibility = Visibility.Collapsed;
+        }
+    }
+}
